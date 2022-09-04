5a07702401e940f49c02972dae2cd3acb9543ca8
Semen <semgenomsk@yandex.ru>
2022-09-04 17:47:19 +0600

    Done task 1

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+int numberA=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число: ");
+int numberB=Convert.ToInt32(Console.ReadLine());
+
+if (numberB*numberB == numberA)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
