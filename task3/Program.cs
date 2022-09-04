43ecd5cf4b3cf65bfa556bdf9ad90a56901002f5
Semen <semgenomsk@yandex.ru>
2022-09-04 19:10:12 +0600

    Done all task2,3,4

---
+++ task3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = -number;
+while (count <= number)
+{
+    Console.Write(count + " ");
+    count++;
+}
---
+++ task4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number < 1000)
+{
+    int count = number % 10;
+    Console.WriteLine(count); 
+}
+else
+{
+    Console.WriteLine("Введено не трехзначное число!");
+}
